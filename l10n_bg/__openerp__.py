7bf6c7a9f41924caf08f8b8c975191f78e758385
Kostadin Kostadinov <isheo@ru-se.com>
2016-03-25 10:47:57 +0200

    odoo bg charts
    Odoo BG charts

---
+++ l10n_bg/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+
+{
+    "name" : "Bulgarian - Accounting",
+    "version" : "1.0",
+    "author" : "Kostadin Kostadinov",
+    "website": "",
+    "category" : "Localization/Account Charts",
+    "depends" : ['account','base_vat'],
+    "description": """
+This is the module to manage the Accounting Chart, VAT structure, Fiscal Position and Tax Mapping.
+================================================================================================================
+
+Bulgarian accounting chart and localization.
+    """,
+    "demo" : [],
+    "data" : [
+              'account_chart.xml',
+              'account_chart_template.yml',
+              ],
+    "installable": True,
+}
