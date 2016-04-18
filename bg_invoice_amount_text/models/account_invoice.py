ba26ca28abd2f217208f0bf580d9c13de4175cf1
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-04-18 18:31:49 +0300

    bg invoice amount text // словом

---
+++ bg_invoice_amount_text/models/account_invoice.py
@@ -0,0 +1,15 @@
+from openerp import models, fields, api, _
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.one
+    @api.depends('amount_total')
+    def _compute_text(self):
+        self.amount_in_word = amount_to_text_bg(self.amount_total, self.currency_id.name)
+
+    amount_in_word = fields.Char(string='Словом: ',
+                                 readonly=True,
+                                 default=False, copy=False, compute='_compute_text'
+                                 )
