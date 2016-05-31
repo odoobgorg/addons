7be94c64a0d5717abb1626e7b9adbb807806cd25
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-05-31 17:48:20 +0300

    #ODOO-31 #comment add some styles

---
+++ theme_semka/__openerp__.py
@@ -0,0 +1,15 @@
+{
+  'name': 'Semka theme',
+  'description': 'Website theme for Semka',
+  'version': '1.0',
+  'author': 'BGO TEAM',
+  'data': [
+      'views/pages.xml',
+      'views/layout.xml',
+      'views/assets.xml'
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+  'installable': True,
+  'application': True,
+}
