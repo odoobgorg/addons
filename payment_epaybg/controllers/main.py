51378cfef274e54ea1f05785a51747fbd188a618
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-06-17 17:43:04 +0300

    note1, note2

--- payment_epaybg/controllers/main.py
+++ payment_epaybg/controllers/main.py
@@ -13,57 +13,18 @@ _logger = logging.getLogger(__name__)
 
 
 class EpaybgController(http.Controller):
-    _return_url = '/shop/confirmation'
-    # _return_url = '/payment/epaybg/return'
+    # _return_url = '/shop/confirmation'
+    _return_url = '/payment/epaybg/return/'
+    _cancel_url = '/payment/epaybg/cancel/'
+    _notify_url = '/payment/epaybg/notification/'
 
-    @http.route([
-        '/payment/epaybg/return',
-    ], type='http', auth='none', csrf=False)
-    def epaybg_return(self, **post):
-        _logger.info('Beginning epaybg_return form_feedback with post data %s', pprint.pformat(post))  # debug
-
+    def _get_return_url(self, **post):
+        """ Extract the return URL from the data coming from epaybg. """
         return_url = post.pop('return_url', '')
-        request.registry['payment.transaction'].form_feedback(request.cr, SUPERUSER_ID, post, 'epaybg', context=request.context)
-
-        # if post.get('authResult') not in ['CANCELLED']:
-        #     request.registry['payment.transaction'].form_feedback(request.cr, SUPERUSER_ID, post, 'epaybg', context=request.context)
-        # return_url = post.pop('return_url', '')
-        # if not return_url:
-        #     custom = json.loads(post.pop('merchantReturnData', '{}'))
-        #     return_url = custom.pop('return_url', '/')
-        return werkzeug.utils.redirect(return_url)
-
-    @http.route([
-        '/payment/epaybg/notification',
-    ], type='http', auth='none', methods=['POST'], csrf=False)
-    def epaybg_notification(self, **post):
-        _logger.info('Beginning epaybg_notification form_feedback with post data %s', pprint.pformat(post))  # debug
-
-        encoded, checksum = post.get('encoded'), post.get('checksum')
-
-        if not encoded or not checksum:
-            return ''
-
-        epay_decoded_result = self.epay_decoded_result(encoded)
-        tx_id = int(epay_decoded_result['INVOICE'])
-
-        tx = request.registry['payment.transaction'].browse(request.cr, SUPERUSER_ID, tx_id, context=request.context)
-
-        if not tx:
-            # XXX if not recognise this invoice
-            status = 'NO'
-        elif epay_decoded_result['STATUS'] == 'PAID':
-            # XXX if OK for this invoice
-            status = 'OK'
-        else:
-            # XXX if error for this invoice
-            status = 'ERR'
-
-        info_data = "INVOICE=%s:STATUS=%s\n" % (tx_id, status)
-
-        _logger.critical(info_data)
-
-        return info_data
+        if not return_url:
+            custom = json.loads(post.pop('custom', False) or '{}')
+            return_url = custom.get('return_url', '/')
+        return return_url
 
     def _epaybg_generate_merchant_decoded(self, encoded):
         return base64.b64decode(encoded)
@@ -78,3 +39,44 @@ class EpaybgController(http.Controller):
                 if len(w) > 0:
                     dict1[w[0]] = w[1]
         return dict1
+
+    @http.route([
+        '/payment/epaybg/return',
+    ], type='http', auth='none', csrf=False)
+    def epaybg_return(self, **post):
+        _logger.info('Beginning epaybg_return form_feedback with post data %s', pprint.pformat(post))  # debug
+        return_url = self._get_return_url(**post)
+        request.registry['payment.transaction'].form_feedback(request.cr, SUPERUSER_ID, post, 'epaybg', context=request.context)
+        return werkzeug.utils.redirect(return_url)
+
+    @http.route('/payment/epaybg/cancel', type='http', auth="none", csrf=False)
+    def epaybg_cancel(self, **post):
+        """ When the user cancels its epaybg payment: GET on this route """
+        cr, uid, context = request.cr, SUPERUSER_ID, request.context
+        _logger.info('Beginning epaybg cancel with post data %s', pprint.pformat(post))  # debug
+        return_url = self._get_return_url(**post)
+        return werkzeug.utils.redirect(return_url)
+
+    @http.route([
+        '/payment/epaybg/notification',
+    ], type='http', auth='none', methods=['POST'], csrf=False)
+    def epaybg_notification(self, **post):
+        _logger.info('Beginning epaybg_notification form_feedback with post data %s', pprint.pformat(post))  # debug
+        encoded, checksum = post.get('encoded'), post.get('checksum')
+        if not encoded or not checksum:
+            return ''
+        epay_decoded_result = self.epay_decoded_result(encoded)
+        tx_id = int(epay_decoded_result['INVOICE'])
+        tx = request.registry['payment.transaction'].browse(request.cr, SUPERUSER_ID, tx_id, context=request.context)
+        if not tx:
+            # XXX if not recognise this invoice
+            status = 'NO'
+        elif epay_decoded_result['STATUS'] == 'PAID':
+            # XXX if OK for this invoice
+            status = 'OK'
+        else:
+            # XXX if error for this invoice
+            status = 'ERR'
+        info_data = "INVOICE=%s:STATUS=%s\n" % (tx_id, status)
+        _logger.critical(info_data)
+        return info_data
