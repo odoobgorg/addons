597a8e8a626e9961365cf1c3029c063f0a237685
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-06-17 14:37:12 +0300

    note1, note2

--- payment_epaybg/controllers/main.py
+++ payment_epaybg/controllers/main.py
@@ -33,29 +33,15 @@ class EpaybgController(http.Controller):
 
         if not tx:
             # XXX if not recognise this invoice
-            info_data = "INVOICE=%s:STATUS=NO\n" % tx_id
-            tx.write({'state': 'pending'})
+            status = 'NO'
         elif epay_decoded_result['STATUS'] == 'PAID':
             # XXX if OK for this invoice
-            info_data = "INVOICE=%s:STATUS=OK\n" % tx_id
-            tx.write({'state': 'done'})
-        elif epay_decoded_result['STATUS'] == 'DENIED' or epay_decoded_result['STATUS'] == 'EXPIRED':
+            status = 'OK'
+        else:
             # XXX if error for this invoice
-            info_data = "INVOICE=%s:STATUS=ERR\n" % tx_id
-            tx.write({'state': 'pending'})
+            status = 'ERR'
 
-        # tx_id = post.get('merchantReference') and request.registry['payment.transaction'].search(request.cr, SUPERUSER_ID, [('reference', 'in', [post.get('merchantReference')])], limit=1, context=request.context)
-        # if post.get('eventCode') in ['AUTHORISATION'] and tx_id:
-        #     tx = request.registry['payment.transaction'].browse(request.cr, SUPERUSER_ID, tx_id, context=request.context)
-        #     states = (post.get('merchantReference'), post.get('success'), tx.state)
-        #     if (post.get('success') == 'true' and tx.state == 'done') or (post.get('success') == 'false' and tx.state in ['cancel', 'error']):
-        #         _logger.info('Notification from epaybg for the reference %s: received %s, state is %s', states)
-        #     else:
-        #         _logger.warning('Notification from epaybg for the reference %s: received %s but state is %s', states)
-
-        _logger.critical("start info_data")
-        _logger.critical(info_data)
-        _logger.critical("end info_data")
+        info_data = "INVOICE=%s:STATUS=%s\n" % tx_id, status
 
         return info_data
 
