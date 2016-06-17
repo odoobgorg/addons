209688afbe21dea7735235de645a04d72da7a7e3
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-06-17 12:17:27 +0300

    note1, note2

--- payment_epaybg/controllers/main.py
+++ payment_epaybg/controllers/main.py
@@ -12,21 +12,7 @@ _logger = logging.getLogger(__name__)
 
 
 class EpaybgController(http.Controller):
-    # _return_url = '/payment/epaybg/return/'
     _return_url = '/shop/confirmation'
-
-    @http.route([
-        '/payment/epaybg/return',
-    ], type='http', auth='none', csrf=False)
-    def epaybg_return(self, **post):
-        _logger.info('Beginning epaybg_return form_feedback with post data %s', pprint.pformat(post))  # debug
-        if post.get('authResult') not in ['CANCELLED']:
-            request.registry['payment.transaction'].form_feedback(request.cr, SUPERUSER_ID, post, 'epaybg', context=request.context)
-        return_url = post.pop('return_url', '')
-        if not return_url:
-            custom = json.loads(post.pop('merchantReturnData', '{}'))
-            return_url = custom.pop('return_url', '/')
-        return werkzeug.utils.redirect(return_url)
 
     @http.route([
         '/payment/epaybg/notification',
@@ -35,18 +21,35 @@ class EpaybgController(http.Controller):
         _logger.info('Beginning epaybg_notification form_feedback with post data %s', pprint.pformat(post))  # debug
 
         encoded, checksum = post.get('encoded'), post.get('checksum')
-        _logger.critical("START TEST")
-        _logger.critical(encoded)
-        _logger.critical(checksum)
-        _logger.critical("END TEST")
 
-        tx_id = post.get('merchantReference') and request.registry['payment.transaction'].search(request.cr, SUPERUSER_ID, [('reference', 'in', [post.get('merchantReference')])], limit=1, context=request.context)
-        if post.get('eventCode') in ['AUTHORISATION'] and tx_id:
-            tx = request.registry['payment.transaction'].browse(request.cr, SUPERUSER_ID, tx_id, context=request.context)
-            states = (post.get('merchantReference'), post.get('success'), tx.state)
-            if (post.get('success') == 'true' and tx.state == 'done') or (post.get('success') == 'false' and tx.state in ['cancel', 'error']):
-                _logger.info('Notification from epaybg for the reference %s: received %s, state is %s', states)
-            else:
-                _logger.warning('Notification from epaybg for the reference %s: received %s but state is %s', states)
+        if not encoded or not checksum:
+            return ''
 
-        return '[accepted]'
+        epay_decoded_result = TxEpaybg.epay_decoded_result(encoded)
+        tx_id = int(epay_decoded_result['INVOICE'])
+
+        tx = request.registry['payment.transaction'].browse(request.cr, SUPERUSER_ID, tx_id, context=request.context)
+
+        if not tx:
+            # XXX if not recognise this invoice
+            info_data = "INVOICE=%s:STATUS=NO\n" % tx_id
+        elif epay_decoded_result['STATUS'] == 'PAID':
+            # XXX if OK for this invoice
+            info_data = "INVOICE=%s:STATUS=OK\n" % tx_id
+        elif epay_decoded_result['STATUS'] == 'DENIED' or epay_decoded_result['STATUS'] == 'EXPIRED':
+            # XXX if error for this invoice
+            info_data = "INVOICE=%s:STATUS=ERR\n" % tx_id
+
+        _logger.critical("info_data")
+        _logger.info(info_data)
+
+        # tx_id = post.get('merchantReference') and request.registry['payment.transaction'].search(request.cr, SUPERUSER_ID, [('reference', 'in', [post.get('merchantReference')])], limit=1, context=request.context)
+        # if post.get('eventCode') in ['AUTHORISATION'] and tx_id:
+        #     tx = request.registry['payment.transaction'].browse(request.cr, SUPERUSER_ID, tx_id, context=request.context)
+        #     states = (post.get('merchantReference'), post.get('success'), tx.state)
+        #     if (post.get('success') == 'true' and tx.state == 'done') or (post.get('success') == 'false' and tx.state in ['cancel', 'error']):
+        #         _logger.info('Notification from epaybg for the reference %s: received %s, state is %s', states)
+        #     else:
+        #         _logger.warning('Notification from epaybg for the reference %s: received %s but state is %s', states)
+
+        return info_data
