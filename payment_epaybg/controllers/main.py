f24714559c4c6992951edf09473b96f2b6869c98
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-06-17 16:27:17 +0300

    note1, note2

--- payment_epaybg/controllers/main.py
+++ payment_epaybg/controllers/main.py
@@ -13,8 +13,8 @@ _logger = logging.getLogger(__name__)
 
 
 class EpaybgController(http.Controller):
-    # _return_url = '/shop/confirmation'
-    _return_url = '/payment/epaybg/return/'
+    _return_url = '/shop/confirmation'
+    # _return_url = '/payment/epaybg/return/'
 
     @http.route([
         '/payment/epaybg/return',
