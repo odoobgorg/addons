1ab2dda0ddd38377b389b4cfc765c11288c32964
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-06-20 18:22:02 +0300

    note1, note2

--- payment_epaybg/models/__init__.py
+++ payment_epaybg/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 import epaybg
+import web_sale
---
+++ payment_epaybg/models/web_sale.py
@@ -0,0 +1,47 @@
+import logging
+_logger = logging.getLogger(__name__)
+
+class websiteSale(http.Controller):
+    def payment_validate(self, transaction_id=None, sale_order_id=None, **post):
+        """ Method that should be called by the server when receiving an update
+        for a transaction. State at this point :
+
+         - UDPATE ME
+        """
+
+        _logger.critical("websiteSale:")
+        _logger.critical(transaction_id)
+
+        cr, uid, context = request.cr, request.uid, request.context
+        email_act = None
+        sale_order_obj = request.registry['sale.order']
+
+        if transaction_id is None:
+            tx = request.website.sale_get_transaction()
+        else:
+            tx = request.registry['payment.transaction'].browse(cr, uid, transaction_id, context=context)
+
+        if sale_order_id is None:
+            order = request.website.sale_get_order(context=context)
+        else:
+            order = request.registry['sale.order'].browse(cr, SUPERUSER_ID, sale_order_id, context=context)
+            assert order.id == request.session.get('sale_last_order_id')
+
+        if not order or (order.amount_total and not tx):
+            return request.redirect('/shop')
+
+        if (not order.amount_total and not tx) or tx.state in ['pending', 'done']:
+            if (not order.amount_total and not tx):
+                # Orders are confirmed by payment transactions, but there is none for free orders,
+                # (e.g. free events), so confirm immediately
+                order.with_context(dict(context, send_email=True)).action_confirm()
+        elif tx and tx.state == 'cancel':
+            # cancel the quotation
+            sale_order_obj.action_cancel(cr, SUPERUSER_ID, [order.id], context=request.context)
+
+        # clean context and session, then redirect to the confirmation page
+        request.website.sale_reset(context=context)
+        if tx and tx.state == 'draft':
+            return request.redirect('/shop')
+
+        return request.redirect('/shop/confirmation')
