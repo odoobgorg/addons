25cce9bd4c085848b3b48a8daa255887cec4b166
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-06-21 12:18:51 +0300

    note1, note2

--- payment_epaybg/models/epaybg.py
+++ payment_epaybg/models/epaybg.py
@@ -174,6 +174,8 @@ class TxEpaybg(osv.Model):
 
     def _epaybg_form_validate(self, cr, uid, tx, data, context=None):
         _logger.info('START _epaybg_form_validate')
+        _logger.info(tx.state)
+
         encoded, checksum = data.get('encoded'), data.get('checksum')
         epay_decoded_result = self.epay_decoded_result(encoded)
         epay_decoded_pformat = pprint.pformat(epay_decoded_result)
