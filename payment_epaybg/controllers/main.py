47bf73e0429c3a807c42ddf350dd84f3dc281f7c
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-06-21 15:32:08 +0300

    note1, note2

--- payment_epaybg/controllers/main.py
+++ payment_epaybg/controllers/main.py
@@ -14,8 +14,7 @@ _logger = logging.getLogger(__name__)
 
 
 class EpaybgController(http.Controller):
-    # _return_url = '/shop/payment/validate'
-    _return_url = '/shop/confirmation'
+    _return_url = '/shop/payment/validate'
 
     @http.route([
         '/payment/epaybg/feedback',
@@ -23,7 +22,7 @@ class EpaybgController(http.Controller):
     def epaybg_form_feedback(self, **post):
         cr, uid, context = request.cr, SUPERUSER_ID, request.context
         _logger.info('Beginning epaybg form_feedback with post data %s', pprint.pformat(post))  # debug
-        # request.registry['payment.transaction'].form_feedback(cr, uid, post, 'epaybg', context)
+        request.registry['payment.transaction'].form_feedback(cr, uid, post, 'epaybg', context)
         return werkzeug.utils.redirect(post.pop('return_url', '/'))
 
     @http.route([
