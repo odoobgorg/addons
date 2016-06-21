6a54543d101602bedd978ac3189dad4ebe65709d
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-06-21 12:41:16 +0300

    note1, note2

--- payment_epaybg/controllers/main.py
+++ payment_epaybg/controllers/main.py
@@ -3,7 +3,7 @@
 # import json
 import logging
 import pprint
-# import werkzeug
+import werkzeug
 # import base64
 import os
 
@@ -14,8 +14,19 @@ _logger = logging.getLogger(__name__)
 
 
 class EpaybgController(http.Controller):
+    _accept_url = '/payment/epaybg/feedback'
+
+    @http.route([
+        '/payment/epaybg/feedback',
+    ], type='http', auth='none', csrf=False)
+    def epaybg_form_feedback(self, **post):
+        cr, uid, context = request.cr, SUPERUSER_ID, request.context
+        _logger.info('Beginning form_feedback with post data %s', pprint.pformat(post))  # debug
+        request.registry['payment.transaction'].form_feedback(cr, uid, post, 'epaybg', context)
+        return werkzeug.utils.redirect(post.pop('return_url', '/'))
+
     # _return_url = '/shop/confirmation'
-    _return_url = '/shop/payment/validate'
+    # _return_url = '/shop/payment/validate'
 
     @http.route([
         '/payment/epaybg/notification',
@@ -25,7 +36,7 @@ class EpaybgController(http.Controller):
 
         # cr, uid, context = request.cr, request.uid, request.context
         # request.registry['payment.transaction']._epaybg_form_get_tx_from_data(cr, uid, post, context=None)
-        request.registry['payment.transaction'].form_feedback(request.cr, SUPERUSER_ID, post, 'epaybg', context=request.context)
+        # request.registry['payment.transaction'].form_feedback(request.cr, SUPERUSER_ID, post, 'epaybg', context=request.context)
 
         epay_decoded_result = request.registry['payment.transaction'].epay_decoded_result(post.get('encoded'))
         status = epay_decoded_result['STATUS'].rstrip(os.linesep)
