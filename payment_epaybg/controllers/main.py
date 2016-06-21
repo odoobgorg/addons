a17287b832cffcfb90bcca7caa1a3350c9a59934
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-06-21 17:48:49 +0300

    note1, note2

--- payment_epaybg/controllers/main.py
+++ payment_epaybg/controllers/main.py
@@ -14,10 +14,10 @@ _logger = logging.getLogger(__name__)
 
 
 class EpaybgController(http.Controller):
-    # _return_url = '/shop/payment/validate'
-    _notify_url = '/payment/epaybg/notification'
-    _return_url = '/payment/epaybg/feedback'
-    _cancel_url = '/payment/epaybg/cancel'
+    _return_url = '/shop/payment/validate'
+    # _notify_url = '/payment/epaybg/notification'
+    # _return_url = '/payment/epaybg/feedback'
+    # _cancel_url = '/payment/epaybg/cancel'
 
     def _get_return_url(self, **post):
         """ Extract the return URL from the data coming from epaybg. """
