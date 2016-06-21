b3bc3be2a54fbfe6fe833c5dfedaeb74eb8da28f
konstantinkonstantinovbgo <konstantin.konstantinov@bgosoftware.com>
2016-06-21 11:52:07 +0300

    note1, note2

--- payment_epaybg/controllers/main.py
+++ payment_epaybg/controllers/main.py
@@ -23,14 +23,12 @@ class EpaybgController(http.Controller):
     def epaybg_form_feedback(self, **post):
         _logger.info('START epaybg_form_feedback with post data %s', pprint.pformat(post))  # debug
 
-        encoded, checksum = post.get('encoded'), post.get('checksum')
-
         cr, uid, context = request.cr, request.uid, request.context
+        request.registry['payment.transaction']._epaybg_form_get_tx_from_data(self, cr, uid, post, context)
 
-        request.registry['payment.transaction']._epaybg_form_get_tx_from_data(self, cr, uid, post, context=None)
         request.registry['payment.transaction'].form_feedback(request.cr, SUPERUSER_ID, post, 'epaybg', context=request.context)
 
-        epay_decoded_result = request.registry['payment.transaction'].epay_decoded_result(encoded)
+        epay_decoded_result = request.registry['payment.transaction'].epay_decoded_result(post.get('encoded'))
         status = epay_decoded_result['STATUS'].rstrip(os.linesep)
         tx_id = epay_decoded_result['INVOICE'].rstrip(os.linesep)
 
