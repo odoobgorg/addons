bee5f27360ebf589a18dfcf992823ec65b0add85
Kaloyan Naumov <kaloyan@lumnus.net>
2016-08-14 13:09:24 +0300

    Module for pre-selecting Country and City on the Checkout form
    Adding Module for pre-selecting Country and City on the Checkout form,
    based on the “Antiun Ingeniería S.L. - Jairo Llopis” OCA Module.

---
+++ website_sale_default_country_city/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# © 2016 Lumnus LTD - Kaloyan Naumov, adding City for pre-selection
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "eCommerce Default Country & City",
+    "summary": "Give a default value for country and city at checkout",
+    "version": "9.0.1.0.0",
+    "category": "Website",
+    "website": "http://www.odoobg.org, http://www.antiun.com",
+    "author": "Antiun Ingeniería S.L., Lumnus LTD, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_sale",
+    ],
+}
