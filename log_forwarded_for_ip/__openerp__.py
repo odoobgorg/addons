ef7fe1d99917c107fbdd24c929c8cd10b9aa9f3d
Kaloyan Naumov <kaloyan@lumnus.net>
2016-08-19 10:48:59 +0300

    Log Forward for IP from OCA/v8
    Porting Log Forward for IP from OCA/v8

---
+++ log_forwarded_for_ip/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015 Aserti Global Solutions
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "X-Forwarded-For IPs in log",
+    "summary": "Displays source IPs in log when behind a reverse proxy",
+    "version": "9.0.1.0.0",
+    "category": "Tools",
+    "website": "https://odoo-community.org/,http://www.odoobg.net",
+    "author": "Aserti Global Solutions, Lumnus LTD, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "uninstall_hook": "restore_address_string"
+}
