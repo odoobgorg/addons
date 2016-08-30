6fd0ae2d28b0adf145d52781297d542a4f9f819b
Konstantin.Konstantinov <konstantin.konstantinov@bgosoftware.com>
2016-08-30 18:26:33 +0300

    ODOO-39 #time 5m place of deal

--- l10n_bg/models/res_company.py
+++ l10n_bg/models/res_company.py
@@ -32,15 +32,7 @@ _logger = logging.getLogger(__name__)
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    # _columns = {
-    #     'bg_uic': fields.related('partner_id', 'bg_uic', string=_('UIC'), type="char", size=13, required=True,
-    #                              help=_('UIC by Bulgarian register agency')),
-    #     'bg_mol': fields.related('partner_id', 'bg_mol', string=_('MOL'), type="char", size=100, required=True,
-    #                              help=_('MOL')),
-    # }
-
     bg_uic = fields.Char(related='partner_id.bg_uic')
     bg_mol = fields.Char(related='partner_id.bg_mol')
-    # bg_invoice_name = fields.Char(related='partner_id.bg_invoice_name')
     rml_header1 = fields.Char(translate=True)
     rml_footer = fields.Text(translate=True)
--- l10n_bg/models/res_partner.py
+++ l10n_bg/models/res_partner.py
@@ -21,6 +21,7 @@
 ##############################################################################
 
 from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
 
 import logging
 
@@ -30,16 +31,9 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    # _columns = {
-    #     'bg_egn': fields.char(string=_('EGN'), size=10, help=_('EGN')),
-    #     'bg_uic': fields.char(string=_('UIC'), size=13, help=_('UIC by Bulgarian register agency')),
-    #     'bg_mol': fields.char(string=_('MOL'), size=100, help=_('MOL')),
-    # }
-
     bg_egn = fields.Char(string=_('EGN'), store=True, size=10, help=_('EGN'))
     bg_uic = fields.Char(string=_('BULSTAT'), store=True, size=13, help=_('BULSTAT by Bulgarian register agency'))
     bg_mol = fields.Char(string=_('MOL'), translate=True, store=True, size=100, help=_('MOL'))
-    # bg_invoice_name = fields.Char(string=_('NAME_FOR_INVOICE'), translate=True, store=True, help=_('Name for Invoice'))
     zip = fields.Char(translate=True)
     city = fields.Char(translate=True)
     street = fields.Char(translate=True)
@@ -53,7 +47,6 @@ class ResPartner(models.Model):
             raise Exception("EGN isn't valid.")
 
     def egn_checker(self, egn):
-
         if not egn:
             return True
 
@@ -120,131 +113,135 @@ class ResPartner(models.Model):
 
         return True
 
-    def generate_place_of_deal(self, res, cr, uid, record, context, cities):
-        city = record.city
-        if record.country_id:
-            city += str(record.country_id)
-        if city not in cities:
-            res.append((record.id, self._display_address(cr, uid, record, without_company=True, context=context)))
-            cities.append(city)
+    # def generate_place_of_deal(self, res, cr, uid, record, context, cities):
+    #     city = record.city
+    #     if record.country_id:
+    #         city += str(record.country_id)
+    #     if city not in cities:
+    #         res.append((record.id, self._display_address(cr, uid, record, without_company=True, context=context)))
+    #         cities.append(city)
 
-    def name_get(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        res = []
+    # def name_get(self, cr, uid, ids, context=None):
+    #     if context is None:
+    #         context = {}
+    #     if isinstance(ids, (int, long)):
+    #         ids = [ids]
+    #     res = []
+    #
+    #     if context.get('show_city'):
+    #         cities = []
+    #         res_parent_id = False
+    #
+    #         users = self.pool.get('res.users')
+    #         current_user = users.browse(cr, uid, uid, context=context)
+    #
+    #         for record in self.browse(cr, uid, ids, context=context):
+    #             if not record.city:
+    #                 continue
+    #             set_city = False
+    #             set_parent_city = False
+    #             if record.id == current_user.partner_id.id:
+    #                 set_city = True
+    #                 if current_user.parent_id.id:
+    #                     set_parent_city = True
+    #             elif record.id == current_user.company_id.partner_id.id:
+    #                 set_city = True
+    #             elif record.parent_id.id == current_user.company_id.partner_id.id:
+    #                 set_city = True
+    #
+    #             if set_city:
+    #                 self.generate_place_of_deal(res, cr, uid, record, context, cities)
+    #             if set_parent_city:
+    #                 self.generate_place_of_deal(res, cr, uid, current_user.parent_id, context, cities)
+    #                 res_parent_id = current_user.parent_id.id
+    #
+    #         if res_parent_id:
+    #             for record in self.browse(cr, uid, ids, context=context):
+    #                 if res_parent_id == record.parent_id.id:
+    #                     self.generate_place_of_deal(res, cr, uid, record, context, cities)
+    #     else:
+    #         types_dict = dict(self.fields_get(cr, uid, context=context)['type']['selection'])
+    #         for record in self.browse(cr, uid, ids, context=context):
+    #             name = record.name or ''
+    #             if record.parent_id and not record.is_company:
+    #                 if not name and record.type in ['invoice', 'delivery', 'other']:
+    #                     name = types_dict[record.type]
+    #                 name = "%s, %s" % (record.parent_name, name)
+    #             if context.get('show_address_only'):
+    #                 name = self._display_address(cr, uid, record, without_company=True, context=context)
+    #             if context.get('show_address'):
+    #                 name = name + "\n" + self._display_address(cr, uid, record, without_company=True, context=context)
+    #             name = name.replace('\n\n', '\n')
+    #             name = name.replace('\n\n', '\n')
+    #             if context.get('show_email') and record.email:
+    #                 name = "%s <%s>" % (name, record.email)
+    #             if context.get('html_format'):
+    #                 name = name.replace('\n', '<br/>')
+    #             res.append((record.id, name))
+    #
+    #     return res
 
-        if context.get('show_city'):
-            cities = []
-            res_parent_id = False
+    # def _display_address(self, cr, uid, address, without_company=False, context=None):
+    #
+    #     '''
+    #     The purpose of this function is to build and return an address formatted accordingly to the
+    #     standards of the country where it belongs.
+    #
+    #     :param address: browse record of the res.partner to format
+    #     :returns: the address formatted in a display that fit its country habits (or the default ones
+    #         if not country is specified)
+    #     :rtype: string
+    #     '''
+    #
+    #     # get the information that will be injected into the display format
+    #     # get the address format
+    #     if context.get('show_city'):
+    #         address_format = "%(city)s, %(country_name)s"
+    #     else:
+    #         address_format = address.country_id.address_format or \
+    #                          "%(street)s\n%(street2)s\n%(city)s %(state_code)s %(zip)s\n%(country_name)s"
+    #
+    #     args = {
+    #         'state_code': address.state_id.code or '',
+    #         'state_name': address.state_id.name or '',
+    #         'country_code': address.country_id.code or '',
+    #         'country_name': address.country_id.name or '',
+    #         'company_name': address.parent_name or '',
+    #     }
+    #
+    #     for field in self._address_fields(cr, uid, context=context):
+    #         args[field] = getattr(address, field) or ''
+    #
+    #     if context.get('show_city'):
+    #         args['state_code'] = ''
+    #         args['state_name'] = ''
+    #         args['country_code'] = ''
+    #         args['company_name'] = ''
+    #     elif without_company:
+    #         args['company_name'] = ''
+    #     elif address.parent_id:
+    #         address_format = '%(company_name)s\n' + address_format
+    #
+    #     return address_format % args
 
-            users = self.pool.get('res.users')
-            current_user = users.browse(cr, uid, uid, context=context)
+    # def _commercial_fields(self, cr, uid, context=None):
+    #     """ Returns the list of fields that are managed by the commercial entity
+    #     to which a partner belongs. These fields are meant to be hidden on
+    #     partners that aren't `commercial entities` themselves, and will be
+    #     delegated to the parent `commercial entity`. The list is meant to be
+    #     extended by inheriting classes. """
+    #     return ['vat', 'credit_limit', 'bg_egn', 'bg_mol', 'bg_uic']
 
-            for record in self.browse(cr, uid, ids, context=context):
-                if not record.city:
-                    continue
-                set_city = False
-                set_parent_city = False
-                if record.id == current_user.partner_id.id:
-                    set_city = True
-                    if current_user.parent_id.id:
-                        set_parent_city = True
-                elif record.id == current_user.company_id.partner_id.id:
-                    set_city = True
-                elif record.parent_id.id == current_user.company_id.partner_id.id:
-                    set_city = True
+    @api.model
+    def _commercial_fields(self):
+        return super(ResPartner, self)._commercial_fields() + \
+            ['bg_egn', 'bg_mol', 'bg_uic']
 
-                if set_city:
-                    self.generate_place_of_deal(res, cr, uid, record, context, cities)
-                if set_parent_city:
-                    self.generate_place_of_deal(res, cr, uid, current_user.parent_id, context, cities)
-                    res_parent_id = current_user.parent_id.id
-
-            if res_parent_id:
-                for record in self.browse(cr, uid, ids, context=context):
-                    if res_parent_id == record.parent_id.id:
-                        self.generate_place_of_deal(res, cr, uid, record, context, cities)
-        else:
-            types_dict = dict(self.fields_get(cr, uid, context=context)['type']['selection'])
-            for record in self.browse(cr, uid, ids, context=context):
-                name = record.name or ''
-                if record.parent_id and not record.is_company:
-                    if not name and record.type in ['invoice', 'delivery', 'other']:
-                        name = types_dict[record.type]
-                    name = "%s, %s" % (record.parent_name, name)
-                if context.get('show_address_only'):
-                    name = self._display_address(cr, uid, record, without_company=True, context=context)
-                if context.get('show_address'):
-                    name = name + "\n" + self._display_address(cr, uid, record, without_company=True, context=context)
-                name = name.replace('\n\n', '\n')
-                name = name.replace('\n\n', '\n')
-                if context.get('show_email') and record.email:
-                    name = "%s <%s>" % (name, record.email)
-                if context.get('html_format'):
-                    name = name.replace('\n', '<br/>')
-                res.append((record.id, name))
-
-        return res
-
-    def _display_address(self, cr, uid, address, without_company=False, context=None):
-
-        '''
-        The purpose of this function is to build and return an address formatted accordingly to the
-        standards of the country where it belongs.
-
-        :param address: browse record of the res.partner to format
-        :returns: the address formatted in a display that fit its country habits (or the default ones
-            if not country is specified)
-        :rtype: string
-        '''
-
-        # get the information that will be injected into the display format
-        # get the address format
-        if context.get('show_city'):
-            address_format = "%(city)s, %(country_name)s"
-        else:
-            address_format = address.country_id.address_format or \
-                             "%(street)s\n%(street2)s\n%(city)s %(state_code)s %(zip)s\n%(country_name)s"
-
-        args = {
-            'state_code': address.state_id.code or '',
-            'state_name': address.state_id.name or '',
-            'country_code': address.country_id.code or '',
-            'country_name': address.country_id.name or '',
-            'company_name': address.parent_name or '',
-        }
-
-        for field in self._address_fields(cr, uid, context=context):
-            args[field] = getattr(address, field) or ''
-
-        if context.get('show_city'):
-            args['state_code'] = ''
-            args['state_name'] = ''
-            args['country_code'] = ''
-            args['company_name'] = ''
-        elif without_company:
-            args['company_name'] = ''
-        elif address.parent_id:
-            address_format = '%(company_name)s\n' + address_format
-
-        return address_format % args
-
-    def _commercial_fields(self, cr, uid, context=None):
-        """ Returns the list of fields that are managed by the commercial entity
-        to which a partner belongs. These fields are meant to be hidden on
-        partners that aren't `commercial entities` themselves, and will be
-        delegated to the parent `commercial entity`. The list is meant to be
-        extended by inheriting classes. """
-        # return ['vat', 'credit_limit', 'bg_egn', 'bg_mol', 'bg_uic', 'bg_invoice_name']
-        return ['vat', 'credit_limit', 'bg_egn', 'bg_mol', 'bg_uic']
-
-    def _display_name_compute(self, cr, uid, ids, name, args, context=None):
-        context = dict(context or {})
-        context.pop('show_address', None)
-        context.pop('show_address_only', None)
-        context.pop('show_email', None)
-        context.pop('show_city', None)
-
-        return dict(self.name_get(cr, uid, ids, context=context))
+    # def _display_name_compute(self, cr, uid, ids, name, args, context=None):
+    #     context = dict(context or {})
+    #     context.pop('show_address', None)
+    #     context.pop('show_address_only', None)
+    #     context.pop('show_email', None)
+    #     context.pop('show_city', None)
+    #
+    #     return dict(self.name_get(cr, uid, ids, context=context))
